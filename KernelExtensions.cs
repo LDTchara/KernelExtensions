94b9fb08e59443bc25bfc9642a1550394cd8cc4e
LDTchara <uekidgh@foxmail.com>
2026-04-11 15:38:28 +0800

    Add files via upload

---
+++ KernelExtensions.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Hacknet;
+
+namespace CustomTrial;
+
+[BepInPlugin(ModGUID, ModName, ModVer)]
+public class KernelExtensions : HacknetPlugin
+{
+    public const string ModGUID = "com.LDTchara.KernelExtensions";
+    public const string ModName = "KernelExtensions";
+    public const string ModVer = "0.0.0";
+
+    public override bool Load()
+    {
+        return true;
+    }
+}
